c112e4dda442fcdfb8912352cf9c1ff8af34756f
Legend <jasonrfelipe@gmail.com>
2018-08-17 15:29:18 -0700

    cleaned up more css on login page, added modals instead of alert, put login controller for encapsulation

---
+++ controllers/loginController.js
@@ -0,0 +1,7 @@
+$('#register-button').on('click', function () {
+    $('#register-modal').modal('toggle')
+});
+
+$('#login-button').on('click', function(){
+    $('#login-modal').modal('toggle')
+});
