d5be5664580d67422177ba6fadabd71bf62f0a2e
Legend <jasonrfelipe@gmail.com>
2018-08-17 16:25:03 -0700

    finalized main page css for mobile, starting login code

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,7 +1,47 @@
-$('#register-button').on('click', function () {
-    $('#register-modal').modal('toggle')
-});
+$(document).ready(function () {
 
-$('#login-button').on('click', function(){
-    $('#login-modal').modal('toggle')
+    const username = $('#userName').val;
+
+
+
+    $('#register-button').on('click', function () {
+        registerUser();
+        $('#register-modal').modal('toggle')
+    });
+
+    $('#login-button').on('click', function () {
+        loginUser();
+        $('#login-modal').modal('toggle')
+    });
+
+
+
+
+
+
+    function loginUser() {
+        console.log('this is a login function');
+        console.log(username)
+        //Stuff to do. 
+        //Keep user Logged in as username (maybe we can use socket.id as a login variable?)
+
+    }
+
+    function registerUser() {
+        console.log('this is a register function');
+
+        const newUser = username;
+        console.log(newUser     )
+
+        //Stuff to do.
+        //Save this the newUser into the database.
+        
+
+    
+
+    }
+
+
+
+
 });
