a14ffd1af2b37a42e0018dcbb6a0def9e56a9566
gladavis89 <gla.davis89@gmail.com>
2018-08-24 12:21:09 -0700

    gd

--- public/assets/js/chat.js
+++ public/assets/js/chat.js
@@ -7,7 +7,7 @@ $(function () {
     $(this).toggleClass('active');
   });
 
-
+});
   //Uses our connection to the server
 
 
@@ -168,9 +168,9 @@ $(function () {
       title: $('#title').val().trim(),
       description: $("#description").val().trim()
     }
-    // addTopic(newTopic, function () {
-    //   $('#homeSubmenu').append("<li id='menuBar'><button class='roomName'>" + newTopic.title + "</button></li>")
-    // });
+    addTopic(newTopic, function () {
+      $('#homeSubmenu').append("<li id='menuBar'><button class='roomName'>" + newTopic.title + "</button></li>")
+    });
   });
 
   function addTopic(newTopic, cb) {
@@ -185,4 +185,3 @@ $(function () {
 
 
 
-});
